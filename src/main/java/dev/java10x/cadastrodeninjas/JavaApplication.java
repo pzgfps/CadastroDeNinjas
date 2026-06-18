c2e2174a40e27e0966d5e37570db693efffd25d4
Pedro Zagato Ogasawara Simoni <pedro.zagato@santamerica.local>
2026-06-18 16:24:46 -0300

    first commit

---
+++ src/main/java/dev/java10x/cadastrodeninjas/JavaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplication.class, args);
+    }
+
+}
