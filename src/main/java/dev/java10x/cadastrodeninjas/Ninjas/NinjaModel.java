37f65044c1d4075b3161bd8b1f70866147b8bce9
Pedro Zagato Ogasawara Simoni <pedro.zagato@santamerica.local>
2026-06-23 16:41:38 -0300

    fix: Coloca a coluna de email como UNIQUE

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -20,6 +20,8 @@ public class NinjaModel extends MissoesModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @Column(unique = true)
     private String email;
     private int idade;
 
