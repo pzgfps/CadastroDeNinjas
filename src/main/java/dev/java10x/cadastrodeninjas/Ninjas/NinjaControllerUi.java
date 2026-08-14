b9c67351c71dc1363edabb42b425d043b8111154
Pedro Zagato Ogasawara Simoni <pedro.zagato@santamerica.local>
2026-08-14 17:54:31 -0300

    Adicionando o controllerUI

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaControllerUi.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaControllerUi.java
@@ -1,4 +1,29 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("ninjas/ui")
 public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+
+
+    @GetMapping("/listar")
+    public String mostrarTodosOsNinjas(Model model) {
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "listarNinjas"; // Tem que retornar o nome da pagina que renderiza "listarNinjas.html"
+    }
+
 }
